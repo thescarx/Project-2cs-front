7bf9024b688fdc7941ab360626e6b8e3432e89b0
adScarX <aa.djellouli@esi-sba.dz>
2023-06-09 19:22:33 +0100

    working on it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px]">
-          <img src={discount} alt="discount" className=" w-[32px] h-[32px]" />
+          <img src={discount} alt="discount" className=" w-[32px] h-[32px] " />
           <p className={`${styles.paragraph} ml-2`}>
             <span className=" text-white">20%</span> DISCOUNT FOR{" "}
             <span className=" text-white">1 MONTH</span> ACCOUNT
@@ -36,7 +36,7 @@ function Hero() {
           annual fees.</p>
       </div>
       <div className={` flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`} >
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5] blend-bg-gray-500 rounded-md" />
         
         <div className=" absolute z-[0] w-[40%] h-[35%] pink__gradient top-0 " />
         <div className=" absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40 " />
